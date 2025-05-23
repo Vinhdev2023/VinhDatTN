9e167dcb00bb80f53a3984e28ee313ac611fafd1
Vinhdev2023 <daothanhvinh2004@gmail.com>
2025-05-23 11:39:11 +0700

    vinh update

--- resources/views/admin/book/books.blade.php
+++ resources/views/admin/book/books.blade.php
@@ -201,20 +201,6 @@
     </div>
     <x-admin.footer></x-admin.footer>
     <x-admin.script>
-        <!-- DataTables  & Plugins -->
-        <script src="/admin_plugin/plugins/datatables/jquery.dataTables.min.js"></script>
-        <script src="/admin_plugin/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-        <script src="/admin_plugin/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-        <script src="/admin_plugin/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-        <script src="/admin_plugin/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-        <script src="/admin_plugin/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-        <script src="/admin_plugin/plugins/jszip/jszip.min.js"></script>
-        <script src="/admin_plugin/plugins/pdfmake/pdfmake.min.js"></script>
-        <script src="/admin_plugin/plugins/pdfmake/vfs_fonts.js"></script>
-        <script src="/admin_plugin/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-        <script src="/admin_plugin/plugins/datatables-buttons/js/buttons.print.min.js"></script>
-        <script src="/admin_plugin/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
-
         <script>
             $(function () {
                 $('#example2').DataTable({
