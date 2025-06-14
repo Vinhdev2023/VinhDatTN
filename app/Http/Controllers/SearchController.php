1dbdfbb780ca53c0fb81ed0914a51ba51267b0ce
Vinhdev2023 <daothanhvinh2004@gmail.com>
2025-06-14 11:40:50 +0700

    vinh update

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -11,17 +11,27 @@ class SearchController extends Controller
         $request->validate([
             'search' => 'required|string|min:1'
         ]);
-        $firstWord = substr($request->search, 0 , strpos($request->search,' '));
-        $stringCut = substr('ab', strpos('ab',' ')+1 , strlen('ab'));
 
-        $arrayWord[] = $firstWord;
-        $arrayWord[] = $firstWord;
-        $arrayWord[] = $firstWord;
+        $stringCut = $request->search;
+        while (strpos($stringCut,' ')) {
+            $arrayWord[] = "title LIKE '%".substr($stringCut, 0, strpos($stringCut, ' '))."%'";
+            $stringCut = substr($stringCut, strpos($stringCut, ' ')+1, strlen($stringCut));
+        }
+        $arrayWord[] = "title LIKE '%".$stringCut."%'";
 
-        dd($request->all(), $firstWord, $stringCut, $arrayWord, strpos('ab',' '));
-        $books = Book::orderBy('created_at', 'desc')->where('quantity', '>', 0)->paginate(12);
+        $sqlLike = '';
+        foreach ($arrayWord as $key => $value) {
+            if ($key == sizeof($arrayWord)-1) {
+                $sqlLike = $sqlLike.$value;
+            }else {
+                $sqlLike = $sqlLike.$value.' AND ';
+            }
+        }
+
+        $books = Book::orderBy('created_at', 'desc')->where('quantity', '>', 0)->whereRaw($sqlLike)->paginate(12);
         $books->load('author');
+        $search = $request->search;
 
-        return view('customer.index', compact('books'));
+        return view('customer.index', compact('books','search'));
     }
 }
--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -39,7 +39,7 @@
                                               <li class="search-menu-opt">
                                                   <div class="iq-search-bar search-book d-flex align-items-center">
                                                       <div class="searchbox">
-                                                          <input type="text" name="search" class="text search-input" placeholder="search here...">
+                                                          <input type="text" name="search" value="{{ isset($search) ? $search : null }}" class="text search-input" placeholder="search here...">
                                                           <a class="search-link" href="#"><i class="ri-search-line"></i></a>
                                                       </div>
                                                       <button type="submit" class="btn btn-primary search-data ml-2">Search</button>
